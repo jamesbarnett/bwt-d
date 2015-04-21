5e711ece351437931db18b2ac8d9d658129c9263
Jim Barnett <james.barnett.jr@gmail.com>
2015-04-21 18:28:03 -0500

    Adds a working implementation of BWT

--- source/bwt.d
+++ source/bwt.d
@@ -2,7 +2,10 @@ module bwt;
 
 import std.stdio;
 import std.algorithm;
+import std.container;
+import std.conv;
 import std.range;
+import std.typecons;
 
 mixin template Bwt() {
   string rotate(string str, int index = 0) {
@@ -16,11 +19,39 @@ mixin template Bwt() {
       ps ~= rotate(str, i);
     }
 
-    return sort(ps);
+    return ps.sort();
   }
 
   string lastColumn(SortedRange!(string[]) ps) {
-    return join(map!(x => x[$])(ps));
+    return array(map!(a => a[$-1 .. $])(ps)).join();
+  }
+
+  Tuple!(string, long) encode(string str) {
+    auto ps = sortedPermutations(str);
+    string lc = lastColumn(ps);
+    long index = countUntil(ps, str);
+    return Tuple!(string, long)(lc, index);
+  }
+
+  string decode(Tuple!(string, long) encoded) {
+    string[] ps = [""];
+    long pindex = 0;
+
+    while (any!(a => a.length < encoded[0].length)(ps)) {
+      for (int i = 0; i < encoded[0].length; ++i) {
+        if (ps.length <= pindex) {
+          ps ~= "";
+          writeln("ps: ", ps);
+        }
+        ps[pindex] = encoded[0][i] ~ ps[pindex];
+        ++pindex;
+      }
+
+      pindex = 0;
+      sort(ps);
+    }
+
+    return ps[encoded[1]];
   }
 }
 
@@ -30,9 +61,7 @@ unittest {
   assert(rotate("banana", 0) == "ananab");
   auto ps = sortedPermutations("banana");
   assert(sortedPermutations("banana").length == "banana".length);
-  string lc = lastColumn(ps);
-
-  /*foreach (string p; ps) {
-    writeln("lastColumn(ps) = ", lc);
-  }*/
+  assert(lastColumn(ps) == "nnbaaa");
+  assert(encode("banana") == Tuple!(string, long)("nnbaaa", 3));
+  assert(decode(Tuple!(string, long)("nnbaaa", 3)) == "banana");
 }
